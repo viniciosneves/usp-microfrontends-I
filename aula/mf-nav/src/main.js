3d10ed9a090cdedd65b53d2332b18b4b250e6d89
Vinicios Neves <vinicios.neves@mac>
2025-03-28 01:05:59 +0000

    passando rotas via message para mf-nav

---
+++ aula/mf-nav/src/main.js
@@ -0,0 +1,15 @@
+const listaDeLinks = document.querySelector('#lista-de-links')
+
+window.addEventListener('message', (evento) => {
+    if (evento.data.type == 'INICIAR') {
+        // console.log(evento.data)
+        evento.data.rotas.forEach(element => {
+            console.log(element)
+            const li = document.createElement('li')
+            const ancora = document.createElement('ancora')
+            ancora.innerText = element.nome
+            li.appendChild(ancora)
+            listaDeLinks.appendChild(li)
+        });
+    }
+})
--- aula/orquestrador/src/main.js
+++ aula/orquestrador/src/main.js
@@ -1,5 +1,27 @@
 import './style.css'
 
+const rotas = [
+    {
+        path: '/',
+        source: '',
+        nome: 'Home'
+    },
+    {
+        path: '/',
+        source: '',
+        nome: 'Produtos'
+    },
+    {
+        path: '/',
+        source: '',
+        nome: 'Clientes'
+    },
+]
+
 const navIframe = document.querySelector('#mf-nav')
+navIframe.onload = () => { // arrow function
+    navIframe.contentWindow.postMessage({ type: 'INICIAR', rotas: rotas }, 'http://localhost:8000')
+}
+
 
 console.log(navIframe)
