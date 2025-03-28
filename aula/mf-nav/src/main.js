126a61bba4648ebc7237c0925bed055f0f6aa28e
Vinicios Neves <vinicios.neves@mac>
2025-03-28 01:59:55 +0000

    finalização do projeto

--- aula/mf-nav/src/main.js
+++ aula/mf-nav/src/main.js
@@ -3,12 +3,18 @@ const listaDeLinks = document.querySelector('#lista-de-links')
 window.addEventListener('message', (evento) => {
     if (evento.data.type == 'INICIAR') {
         // console.log(evento.data)
-        evento.data.rotas.forEach(element => {
-            console.log(element)
+        evento.data.rotas.forEach(rota => {
+            console.log(rota)
             const li = document.createElement('li')
             const ancora = document.createElement('a')
-            ancora.setAttribute('href', element.path)
-            ancora.innerText = element.nome
+            ancora.setAttribute('href', rota.path)
+            ancora.innerText = rota.nome
+
+            ancora.addEventListener('click', (evt) => {
+                evt.preventDefault()
+                window.parent.postMessage({ type: 'NAVEGACAO', destino: rota }, '*')
+            })
+
             li.appendChild(ancora)
             listaDeLinks.appendChild(li)
         });
--- aula/orquestrador/src/main.js
+++ aula/orquestrador/src/main.js
@@ -3,25 +3,52 @@ import './style.css'
 const rotas = [
     {
         path: '/',
-        source: '',
+        source: 'http://localhost:8002/',
         nome: 'Home'
     },
     {
         path: '/produtos',
-        source: '',
+        source: 'http://localhost:8003/',
         nome: 'Produtos'
     },
     {
         path: '/clientes',
-        source: '',
+        source: 'http://localhost:8001/',
         nome: 'Clientes'
     },
 ]
 
 const navIframe = document.querySelector('#mf-nav')
+const containerIframe = document.querySelector('#container')
+
+const pathAtual = window.location.pathname
+
+const mfInicial = rotas.find(r => r.path == pathAtual)
+if (mfInicial) {
+    containerIframe.setAttribute('src', mfInicial.source)
+}
+
 navIframe.onload = () => { // arrow function
     navIframe.contentWindow.postMessage({ type: 'INICIAR', rotas: rotas }, 'http://localhost:8000')
 }
 
+window.addEventListener('message', (evento) => {
+    // evento.origin = 'url.maliciosa.com
+    if (evento.data.type == 'NAVEGACAO') {
+        console.log('Hora de navegar para', evento.data.destino)
+        const mfAlvoNavegacao = rotas.find(r => r.path == evento.data.destino.path)
+        if (mfAlvoNavegacao) {
+            containerIframe.setAttribute('src', mfAlvoNavegacao.source)
+            window.history.pushState({}, '', mfAlvoNavegacao.path)
+        }
+    }
+})
 
-console.log(navIframe)
+window.addEventListener('popstate', () => {
+    const path = window.location.pathname
+    const mfAlvoPop = rotas.find(r => r.path == path)
+    if (mfAlvoPop) {
+        containerIframe.setAttribute('src', mfAlvoPop.source)
+        console.log('pop sendo feito')
+    }
+})
